540387284d7c454d4a1e219f8f2a235fd2085304
siam1113 <hasan.2017.siam@gmail.com>
2024-03-30 19:28:32 +0600

    fix: export error

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@repo/ui";
 import { useRouter } from "next/navigation";
 
 export function NavBar() {
@@ -14,6 +13,6 @@ export function NavBar() {
       <li className="p-3 text-white border border-primary rounded text-lg font-medium bg-green-700 me-1 cursor-pointer" onClick={() => router.push("/find/individual")}>Find Individual</li>
       <li className="p-3 text-white border border-primary rounded text-lg font-medium bg-green-700 me-1 cursor-pointer" onClick={() => router.push("/profile")}>Your Profile</li>
     </ul>
-    <Button color="p-3 text-white border border-primary rounded text-lg font-medium bg-green-600" onClick={handleLogout} >Logout</Button>
+    <button className="p-3 text-white border border-primary rounded text-lg font-medium bg-green-600" onClick={handleLogout} >Logout</button>
   </div>
 }
